f83e1117ecbdb3e69f50236146b6691245d42161
Ravil Galeyev <dehasi@yandex.ru>
2021-06-26 21:58:12 +0200

    STEP-0: Add deps

---
+++ src/main/kotlin/me/dehasi/httpapi/ApplicationKt.kt
@@ -0,0 +1,10 @@
+package me.dehasi.httpapi
+
+import io.ktor.application.Application
+
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+
+}
