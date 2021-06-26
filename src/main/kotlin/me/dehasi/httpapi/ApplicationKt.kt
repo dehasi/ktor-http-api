f20c8a732675e8bc659781a14696e75a0d5743b1
Ravil Galeyev <dehasi@yandex.ru>
2021-06-26 22:19:00 +0200

    STEP-1.1

--- src/main/kotlin/me/dehasi/httpapi/ApplicationKt.kt
+++ src/main/kotlin/me/dehasi/httpapi/ApplicationKt.kt
@@ -3,7 +3,9 @@ package me.dehasi.httpapi
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
+import io.ktor.routing.routing
 import io.ktor.serialization.json
+import me.dehasi.httpapi.models.customerRouting
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -11,5 +13,12 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     install(ContentNegotiation) {
         json()
+    }
+    registerCustomerRoutes()
+}
+
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
     }
 }
--- src/main/kotlin/me/dehasi/httpapi/models/CustomerRoutes.kt
+++ src/main/kotlin/me/dehasi/httpapi/models/CustomerRoutes.kt
@@ -1,8 +1,13 @@
 package me.dehasi.httpapi.models
 
+import Customer
 import customerStorage
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode.Companion.Accepted
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Created
 import io.ktor.http.HttpStatusCode.Companion.NotFound
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
@@ -17,8 +22,20 @@ fun Route.customerRouting() = {
             if (customerStorage.isNotEmpty()) call.respond(customerStorage)
             else call.respondText("not found", status = NotFound)
         }
-        get("{id}") {}
-        post { }
-        delete("{id}") {}
+        get("{id}") {
+            val id = call.parameters["id"] ?: return@get call.respondText("id is needed", status = BadRequest)
+            val customer = customerStorage.find { it.id == id } ?: return@get call.respond(status = NotFound, "")
+            call.respond(customer)
+        }
+        post {
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respond(status = Created, "")
+        }
+        delete("{id}") {
+            val id = call.parameters["id"] ?: return@delete call.respondText("id is needed", status = BadRequest)
+            if (customerStorage.removeIf { it.id == id }) call.respondText("Customer removed", status = Accepted)
+            else call.respond(status = NotFound, "")
+        }
     }
 }
